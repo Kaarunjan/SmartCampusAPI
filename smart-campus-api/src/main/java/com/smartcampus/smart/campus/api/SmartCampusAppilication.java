c62bfe24c7535f4905e9e9698712479f1aa51cac
Kaarunjan Sathiyaseelan <177406112+Kaarunjan@users.noreply.github.com>
2026-04-21 22:09:04 +0530

    Set up the main projects place and imported the necessary dependencies to pom.xml
    Imported the dependencies needed to run the server and the application with jersy and tomcat

---
+++ smart-campus-api/src/main/java/com/smartcampus/smart/campus/api/SmartCampusAppilication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.smart.campus.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Karunyan
+ */
+@ApplicationPath("/api/v1")
+public class SmartCampusAppilication extends Application{
+    
+}
