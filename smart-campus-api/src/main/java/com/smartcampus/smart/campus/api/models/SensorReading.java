c8d721a60109c226b5f580c2f6a08676b7e68ed3
Kaarunjan Sathiyaseelan <177406112+Kaarunjan@users.noreply.github.com>
2026-04-22 11:10:55 +0530

    Setup the models and the encapsualted properly

---
+++ smart-campus-api/src/main/java/com/smartcampus/smart/campus/api/models/SensorReading.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.smart.campus.api.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Karunyan
+ */
+public class SensorReading {
+
+    private String id; // Unique reading event ID (UUID recommended)
+    private long timestamp; // Epoch time (ms) when the reading was captured
+    private double value;
+    
+    public SensorReading(String id, long timestamp, double value)
+    {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+    
+    
+    
+}
